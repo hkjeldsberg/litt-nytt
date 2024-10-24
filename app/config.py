3e840fb07ad631edaf94cbb4b818faec49191530
Henrik Kjedsberg <henrik.kjeldsberg@live.no>
2024-10-24 09:49:02 +0200

    Read port

--- app/config.py
+++ app/config.py
@@ -1,5 +1,11 @@
+from starlette.config import Config
+
+config = Config(".env")
+
+ACCOUNT_URL = config("PREDICTION_SERVICE_BLOB_STORAGE_ACCOUNT_URL", default=None)
+
 APP_NAME = "TinyNews"
 APP_VERSION = "0.1.0"
-APP_PORT = 8000
+APP_PORT = config("PORT", default=8000)
 APP_HOST = "127.0.0.1"
 APP_TIMEOUT = 60
